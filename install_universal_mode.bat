aadd01bb47c68c949f067823f84d9a5310ec56b6
dong.baocheng <dong.baocheng@sincetimes.com>
2024-06-13 11:36:34 +0800

    修改说明

--- install_universal_mode.bat
+++ install_universal_mode.bat
@@ -1,6 +1,6 @@
 del /f install.apks
 
-rem 模式为universal，否则有些收集安装后可能找不到链接库libmain.so（比如一加手机就需要用universal模式）
+rem 模式为universal，否则有些手机安装后可能找不到链接库libmain.so（比如一加手机就需要用universal模式）
 java -jar bundletool-all-1.16.0.jar build-apks --bundle=install.aab --output=install.apks --mode=universal --ks=user.keystore --ks-pass=pass:got20240201 --ks-key-alias=got --key-pass=pass:got20240201
 
 java -jar bundletool-all-1.16.0.jar install-apks --apks=install.apks
